a08d9a8008e2b21ce3a9c48daa94e15c45f0ef52
JT <joseph.triolo2@gmail.com>
2021-02-12 17:23:55 -0500

    added home

---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Faux Stocks</h1>
+    <router-link to="/login">Login</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
