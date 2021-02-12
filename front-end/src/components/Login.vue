8eaeac38ee8e7a77431eda73af0a63017ebf83ee
JT <joseph.triolo2@gmail.com>
2021-02-12 17:25:06 -0500

    added login

---
+++ front-end/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
